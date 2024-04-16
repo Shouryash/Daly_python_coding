69510f453b70b001f20925877357b1cc11843952
Shouryash <129163161+Shouryash@users.noreply.github.com>
2024-04-16 23:10:12 +0530

    Add files via upload

---
+++ Reverse.py
@@ -0,0 +1,12 @@
+
+#Reversing the worls in a given Striing
+Word=input("Enter sentence : ")
+Word2=Word.split()
+print(Word2)
+Word3=Word2[::-1]
+print(Word3)
+output=" ".join(Word3)
+print(output)
+
+# input :i love algorithm
+# outupt :  algorithm love i
